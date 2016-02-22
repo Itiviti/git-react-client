3dbfe2df6933b2b1771d54b1ffa04ffaa084d9c3
Terry Wong <terry.wong@ullink.com>
2016-02-22 14:30:10 +0800

    Cleanup formatting

--- js/components/GitGrep.js
+++ js/components/GitGrep.js
@@ -1,28 +1,32 @@
 import React from 'react';
 import Rx from 'rx';
-import Spinner from "react-spinkit";
+import Spinner from 'react-spinkit';
 import GrepResult from './GrepResult.js';
-require('../../css/components/GitGrep.css');
-import { renderNodesForLayout, rxFlow, tranformDataForLayout } from './GitCommon.js';
-import { browserHistory } from 'react-router'
+import '../../css/components/GitGrep.css';
+import {renderNodesForLayout, rxFlow, tranformDataForLayout} from './GitCommon.js';
+import {browserHistory} from 'react-router'
 import AppSettings from '../../settings.js';
 import Cookie from 'react-cookie';
 
 class Settings extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { layout: Cookie.load('layout') || 'compact' };
+    this.state = {layout: Cookie.load('layout') || 'compact'};
     this.props.settingsUpdated(this.state);
   }
+
   handleClick() {
-    var layout = this.state.layout == 'google' ? 'compact' : 'google';
+    var layout = this.state.layout === 'google' ? 'compact' : 'google';
     this.setState({layout: layout});
     this.props.settingsUpdated({layout});
     Cookie.save('layout', layout);
   }
+
   render() {
     return (
-      <div style={{marginLeft: 'auto'}} >Layout: <a onClick={this.handleClick.bind(this)}>{this.state.layout}</a></div>
+      <div style={{marginLeft: 'auto'}}>Layout:
+        <a onClick={this.handleClick.bind(this)}>{this.state.layout}</a>
+      </div>
     );
   }
 }
@@ -31,48 +35,71 @@ export default class GrepBox extends React.Component {
   constructor(props) {
     super(props);
     var query = this.props.location.query || {};
-    this.state = {orig: [], repo: query.repo || query.project || '^ul', text: query.text || query.grep || '', branch: query.branch || query.ref || 'HEAD', path: query.path || '.', data: [], pending: false, layout: 'compact'};
+    this.state = {
+      orig: [],
+      repo: query.repo || query.project || '^ul',
+      text: query.text || query.grep || '',
+      branch: query.branch || query.ref || 'HEAD',
+      path: query.path || '.',
+      data: [],
+      pending: false,
+      layout: 'compact'
+    };
   }
+
   loadGrepFromServer(params) {
     this.setState({orig: [], data: [], pending: true});
     var qry = new Rx.Subject();
-    var esc = Rx.Observable.fromEvent(document, 'keydown').filter(e => e.keyCode == 27);
-    var rxQty = rxFlow(`${AppSettings.gitRestApi()}/repo/${params.repo}/grep/${params.branch}?q=${params.text}&path=${params.path}&delimiter=${'%0A%0A'}`, { withCredentials: false })
-        .bufferWithTimeOrCount(500, 10)
-        .map(elt => this.state.orig.concat(elt))
-        .map(orig => ({ orig, data: tranformDataForLayout(orig, this.state.layout) }))
-        .takeUntil(esc)
-        .finally(() => this.setState({pending: false}))
-        .subscribe(this.setState.bind(this));
+    var esc = Rx.Observable.fromEvent(document, 'keydown')
+      .filter(e => e.keyCode === 27);
+    var rxQty = rxFlow(
+        `${AppSettings.gitRestApi()}/repo/${params.repo}/grep/${params.branch}?q=${params.text}&path=${params.path}&delimiter=${'%0A%0A'}`,
+        {withCredentials: false})
+      .bufferWithTimeOrCount(500, 10)
+      .map(elt => this.state.orig.concat(elt))
+      .map(orig => ({orig, data: tranformDataForLayout(orig, this.state.layout)}))
+      .takeUntil(esc)
+      .finally(() => this.setState({pending: false}))
+      .subscribe(this.setState.bind(this));
   }
+
   handleClick(e) {
     e.preventDefault();
-    var subState = ({ path, text, branch, repo}) => ({path, text, branch, repo, submit: 'Grep'});
+    var subState = ({path, text, branch, repo}) => ({
+      path, text, branch, repo, submit: 'Grep'
+    });
     Object.assign(this.props.location.query, subState(this.state));
     browserHistory.replace(this.props.location);
     this.loadGrepFromServer(this.state);
   }
+
   handleAnyChange(name, e) {
     this.setState({[name]: e.target.value});
   }
+
   componentDidMount() {
     var query = this.props.location.query || {};
-    if (query.submit == 'Grep') {
+    if (query.submit === 'Grep') {
       this.loadGrepFromServer(this.state);
     }
   }
+
   settingsUpdated(settings) {
-    if (settings.layout != this.state.layout)
-      this.setState({layout: settings.layout, data: tranformDataForLayout(this.state.orig, settings.layout)});
+    if (settings.layout !== this.state.layout)
+    this.setState({
+      layout: settings.layout,
+      data: tranformDataForLayout(this.state.orig, settings.layout)
+    });
   }
+
   render() {
-    var loading = this.state.pending ? ( <Spinner spinnerName='circle' noFadeIn /> ) : ( <div/> );
+    var loading = this.state.pending ? <Spinner spinnerName='circle' noFadeIn /> : <div/>;
     var grepNodes = renderNodesForLayout(this.state.data, this.state.layout);
     return (
       <div>
         <div style={{background: 'white', display: 'flex'}}>
           <form className="grepForm">
-            <input type="search" placeholder="Matching repos (e.g. ul)" value={this.state.repo} onChange={this.handleAnyChange.bind(this,'repo')} />
+            <input type="search" placeholder="Matching repos (e.g. ul)" value={this.state.repo} onChange={this.handleAnyChange.bind(this, 'repo')} />
             <input type="search" placeholder="Search expression" value={this.state.text} onChange={this.handleAnyChange.bind(this, 'text')} />
             <input type="search" placeholder="Matching branches (e.g. HEAD)" value={this.state.branch} onChange={this.handleAnyChange.bind(this, 'branch')} />
             <input type="search" placeholder="Matching path (e.g. *.java)" value={this.state.path} onChange={this.handleAnyChange.bind(this, 'path')} />
@@ -88,4 +115,3 @@ export default class GrepBox extends React.Component {
     );
   }
 }
-
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,33 @@
-var webpack = require('webpack');  
-module.exports = {  
-    entry: [
-      'webpack/hot/only-dev-server',
-      "./js/app.js"
-    ],
-    output: {
-        path: __dirname + '/build',
-        filename: "bundle.js"
-    },
-    module: {
-      loaders: [
-        { test: /\.js$/, exclude: /node_modules/, loader: "babel", query: {presets:['react','es2015']}},
-        { test: /\.css$/, loader: "style!css" }
-      ]
-    },
-    plugins: [
-      new webpack.NoErrorsPlugin(),
-      new webpack.ProvidePlugin({ 'fetch': 'imports?this=>global!exports?global.fetch!whatwg-fetch' })
-    ],
-    devServer: {
-          historyApiFallback: true
-    }
-
-
+var webpack = require('webpack');
+module.exports = {
+  entry: [
+    'webpack/hot/only-dev-server',
+    './js/app.js'
+  ],
+  output: {
+    path: __dirname + '/build',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['react', 'es2015']
+      }
+    }, {
+      test: /\.css$/,
+      loader: 'style!css'
+    }]
+  },
+  plugins: [
+    new webpack.NoErrorsPlugin(),
+    new webpack.ProvidePlugin({
+      'fetch': 'imports?this=>global!exports?global.fetch!whatwg-fetch'
+    })
+  ],
+  devServer: {
+    historyApiFallback: true
+  }
 };
