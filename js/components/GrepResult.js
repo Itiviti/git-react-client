fb83aa676954c1933fcc8785eb2bde9db329e06b
Terry Wong <terry.wong@ullink.com>
2016-03-02 13:06:32 +0800

    Keep different fromat in separate components

--- js/components/GrepResult.js
+++ js/components/GrepResult.js
@@ -1,39 +1,53 @@
 import React from 'react';
-import '../../css/prism.css';
-import '../prism.js';
 import {PrismCode} from 'react-prism';
-import '../../css/components/GitGrep.css';
 import AppSettings from '../../settings.js';
+import '../prism.js';
+import '../../css/prism.css';
+import '../../css/components/GitGrep.css';
 
-export default (props) => {
+
+const CodeLineGoogleFormat = (props) => {
   const gitViewer = AppSettings.gitViewer();
-  const repoHeader = <a href={gitViewer.viewerForRepo(props)}>
-    {props.repo.replace(/\.git$/, '')}
-  </a>;
+  return <div>
+    <a href={gitViewer.viewerForBranch(props)}>{props.branch}</a>:
+    <a href={gitViewer.viewerForPath(props)}>{props.file}</a>:
+    <a href={gitViewer.viewerForLine(props)}>{props.line_no}</a>:
+    <PrismCode className={determineLang(props.file)}>{props.line}</PrismCode>
+  </div>;
+};
 
-  if (props.file) {
-    let arr = props.file.split('.');
+const CodeLineCompactFormat = (props) => {
+  const gitViewer = AppSettings.gitViewer();
+  return <div>
+    <a href={gitViewer.viewerForRepo(props)}>{props.repo.replace(/\.git$/, '')}</a>
+    <a href={gitViewer.viewerForBranch(props)}>{props.branch}</a>:
+    <a href={gitViewer.viewerForPath(props)}>{props.file}</a>:
+    <a href={gitViewer.viewerForLine(props)}>{props.line_no}</a>:
+    <PrismCode className={determineLang(props.file)}>{props.line}</PrismCode>
+  </div>;
+};
+
+function determineLang(file) {
+  const langs = {
+    cs: 'csharp',
+    fs: 'fsharp',
+    js: 'javascript',
+    md: 'markdown',
+    pl: 'perl',
+    py: 'python',
+    gradle: 'groovy'
+  };
+
+  if (file) {
+    let arr = file.split('.');
     let ext = arr[arr.length - 1];
-    var lang = `language-${langs[ext] || ext}`;
+    return `language-${langs[ext] || ext}`;
   }
-
-  return (
-    <div>
-      {props.layout === 'google' ? '' : repoHeader}:
-      <a href={gitViewer.viewerForBranch(props)}>{props.branch}</a>:
-      <a href={gitViewer.viewerForPath(props)}>{props.file}</a>:
-      <a href={gitViewer.viewerForLine(props)}>{props.line_no}</a>:
-      <PrismCode className={lang}>{props.line}</PrismCode>
-    </div>
-  );
 }
 
-const langs = {
-  cs: 'csharp',
-  fs: 'fsharp',
-  js: 'javascript',
-  md: 'markdown',
-  pl: 'perl',
-  py: 'python',
-  gradle: 'groovy'
+export default ({layout, ...props}) => {
+  const format = {
+    google: CodeLineGoogleFormat
+  }[layout] || CodeLineCompactFormat;
+  return React.createElement(format, props);
 };
