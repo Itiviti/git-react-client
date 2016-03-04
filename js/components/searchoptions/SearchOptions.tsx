b5837571330c4a1b14cebcbcb0674ff7a370a4b3
Remy Masson <muryoh@gmail.com>
2016-03-04 16:53:32 +0100

    Extract OptionsBase
    - SearchOptions now extend OptionsBase
    - That way we have room for GrepOptions

---
+++ js/components/searchoptions/SearchOptions.tsx
@@ -0,0 +1,52 @@
+import * as React from 'react'
+import assign = require('object-assign')
+import TextField = require('material-ui/lib/text-field')
+import RaisedButton = require('material-ui/lib/raised-button')
+import SelectField = require('material-ui/lib/select-field')
+import MenuItem = require('material-ui/lib/menus/menu-item')
+import Toggle = require('material-ui/lib/toggle')
+import Dialog = require('material-ui/lib/dialog')
+import {OptionsBase, OptionsBaseState} from './OptionsBase.tsx'
+
+export default class SearchOptions extends OptionsBase<{location: any, onValidate: any, onRequestClose: any, show: boolean}, SearchOptionsState> {
+  constructor(props) {
+    super(props);
+    var query = this.props.location.query || {};
+    this.state = {
+      mode: query.mode || SearchOptions.MODE_FILE,
+      redirect: query.redirect? Boolean(query.redirect) : false,
+    } as SearchOptionsState;
+    super.initState();
+  }
+
+  public static get MODE_FILE():string { return 'file' }
+  public static get MODE_STACK_FRAME():string { return 'stackframe' }
+
+  handleAnyChange(name, e) {
+    super.setState({[name]: e.target.value} as SearchOptionsState);
+  }
+
+  handleModeChanged(event, index, mode) {
+    super.setState({mode} as SearchOptionsState);
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+    this.props.onValidate(this.state)
+  }
+
+  protected getPostFields() {
+    return <div>
+      <SelectField floatingLabelText="Search mode" value={this.state.mode} onChange={this.handleModeChanged.bind(this)}>
+        <MenuItem value={SearchOptions.MODE_FILE} primaryText={SearchOptions.MODE_FILE}/>
+        <MenuItem value={SearchOptions.MODE_STACK_FRAME} primaryText={SearchOptions.MODE_STACK_FRAME}/>
+      </SelectField><br/>
+      <Toggle label="Auto redirect" defaultToggled={this.state.redirect} onToggle={(e, open) => super.setState({redirect: open} as SearchOptionsState)}/><br />
+    </div>
+  }
+}
+
+export interface SearchOptionsState extends OptionsBaseState {
+  mode: string;
+  redirect: boolean;
+}
